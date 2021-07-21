6b16a598d4e782a80ddcac1232375413b84beff0
xinqinew <xinqinew@163.com>
2021-07-21 18:09:56 +0800

    4th

--- FindCodeBase/FINDCODE.cpp
+++ FindCodeBase/FINDCODE.cpp
@@ -172,3 +172,94 @@ SIZE_T CFINDCODE::GetExeSize();
 	return nSize;
 }
 
+//读取模块至缓存
+SIZE_T CFINDCODE::ReadExeToBuf()
+{
+	SIZE_T 实际读取大小=0;
+	HANDLE 进程句柄=GetGameHp();
+	UINT_PTR 模块基址=GetExeBase();
+	ReadProcessMemory(进程句柄,(LPCVOID)模块基址,ExeBuf,GetExeSize(),&实际读取大小);
+	CloseHandle(进程句柄);
+	return 实际读取大小;
+}
+
+//内存对比 
+UINT_PTR myMemcmp(UINT_PTR str1,UINT_PTR str2,SIZE_T nsize)
+{
+	SIZE_T size8=nsize/8;	//按8字节比较
+	SIZE_T size1=nsize%8; 	//按1字节比较 
+	UINT64* p8a=(UINT64*)strl;
+	UINT64* p8b=(UINT64*)str2;
+
+	BYTE* p1a=(BYTE*)str1;
+	BYTE* p1b=(BYTE*)str2;
+	for (SIZE_T i=0;i<size8;i++) 
+	{
+		if (p8a[i]!=p8b[i]) 
+		{
+			return 1;
+		}
+	}
+	//如果8字节的比较全都相等才会执行到下边 
+	for (SIZE_T i=0;i<size1;i++)
+	{
+		if (p1a[i]!=p1b[i])
+		{
+			return 1;
+		}
+	}
+	//执行到这里 表示全都相等
+	return 0;
+
+}
+//定位字节集特征码
+UINT_PTR CFINDCODE::FindCode(BYTE*ByFindData,BYTE nSize)
+{
+	//缓冲区先把目标进程内存一次读取到我们自己的缓冲区
+	SIZE_T 实际读取大小=ReadExeToBuf();
+	if (实际读取大小! =GetExeSize())
+	(
+		printf("暂停提示:实际读取大小=%llx,GetExeSize()=%llx \r\n",实际读取大小,GetExeSize());
+		getchar();
+	)
+	printf ("开始搜索特征码 实际读里大小=%11x GetExeSize()=%llx nSize=%X\r\n",实际读取大小,GetExeSize(),nSize);
+	//memcmp
+	SIZE_T 偏移=0;
+	//94E230
+	for (UINT_PTR i=0;i<GetExeSize();i++)
+	{
+	
+		//94E230
+		UINT_PTR br=myMemcmp((UINT64)&ExeBuf[i],(UINT64)ByFindData,nSize);
+		if (br==0)
+		{
+			偏移=i;
+			printf("findCode OK i=%d \r\n",i);
+			break;
+		}
+		//printf("i=%llX br=%d\r\n",i,br);
+
+	return GetExeBase()+偏移;
+}
+
+//VirtualQueryEx:查询模块 可以执页面范围
+SIZE_T CFINDCODE::VirtualQueryExe()
+{
+	HANDLE 进程句柄=GetGameHp();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- FindCodeBase/main.cpp
+++ FindCodeBase/main.cpp
@@ -1,8 +1,13 @@
 #include "遍历模块.h"
 #include "FINDCODE.h"
-
+#include "RMEM.h"
 
 CFINDCODE find;
+
+BYTE 特征码20FA0[]=
+{
+	0xA0,0x0F,0x02 //
+}
 
 int main(int argn, char  *argv[])
 {
@@ -13,6 +18,17 @@ int main(int argn, char  *argv[])
 	UINT_PTR 主模块结束地址 = find.GetExeEnd();
 	printf("主模块地址=%llX \r\n开始地址=%llX \r\n结束地址=%llX \r\n主模块大小=%llX",
 		主模块地址,主模块开始地址,主模块结束地址,主模块大小);
+
+	UINT64 max64=0x0007ffffffffffff;
+	printf("%lld 倍\r\n",max64/0x1FA0000);
+	find.VirtualQueryExe();
+	//查找特征码 定位偏移
+	UINT_PTR 找到的地址=find.FindCode(特征码20FA0,sizeof(特征码20FA0));
+
+	printf("偏移所在地址=%p 偏移=%X \r\n",找到的地址,R4(找到的地址));
+
 	getchar();//控制台内暂停用的
 	return 1;
 }
+
+ 
